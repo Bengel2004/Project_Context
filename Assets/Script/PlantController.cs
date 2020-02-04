5b6a3a2f83e053ce8751b007a2c203190a4ec1de
Niels P <niels.poelder@gmail.com>
2020-02-04 14:49:14 +0100

    Added Cinemachine camera controller

---
+++ Assets/Script/PlantController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Vector2 minDeadZone;
+    [SerializeField]
+    private Vector2 maxDeadZone;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //   if(ray.origin.x > minDeadZone.x && ray.origin.y > minDeadZone.y && ray.origin.x < maxDeadZone.x && ray.origin.y < maxDeadZone.y)
+        Vector3 temp = new Vector3(ray.origin.x, ray.origin.y, 0f);
+        transform.position = temp;
+    }
+}
