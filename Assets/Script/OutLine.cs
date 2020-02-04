5dffa4a07a60f46dfd2da2f6295dba802d9cab05
Bun_E <dudcks779425@gmail.com>
2020-02-04 21:02:31 +0900

    First

---
+++ Assets/Script/OutLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutLine : MonoBehaviour
+{
+    SpriteRenderer srenderer;
+    MaterialPropertyBlock mtProperty;
+
+    private void Awake()
+    {
+        srenderer = GetComponent<SpriteRenderer>();
+        mtProperty = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        if (OutLineCtrl.Inst.CompareHitObject(this.gameObject))
+        {
+            StartCoroutine(ISetOutLineMaterial(true));
+        }
+        else
+            StartCoroutine(ISetOutLineMaterial(false));
+    }
+
+    private IEnumerator ISetOutLineMaterial(bool enable)
+    {
+        srenderer.GetPropertyBlock(mtProperty);
+        mtProperty.SetFloat("_OutlineEnabled", System.Convert.ToInt32(enable));
+        srenderer.SetPropertyBlock(mtProperty);
+        yield break;
+    }
+
+}
---
+++ Assets/Script/SelectBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SelectBox : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    [SerializeField] private int index;
+    [SerializeField] [Range(0.8f, 0.9f)] private float size = 0.9f;
+
+    private RectTransform rt;
+    private Image boxImage;
+    private Text boxText;
+
+    private string originText;
+
+    private void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+
+        boxText = transform.Find("[Text]").GetComponent<Text>();
+        boxImage = transform.Find("[Image]").GetComponent<Image>();
+
+        originText = boxText.text;
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        rt.localScale = Vector3.one;
+        boxText.text = originText;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        rt.localScale = Vector3.one * size;
+        boxText.text = "Push!";
+    }
+}
