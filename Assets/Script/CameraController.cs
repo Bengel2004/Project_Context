ca233cbf55886af10aee43d385280d5ba99840b0
Niels <niels.poelder@gmail.com>
2020-02-06 14:23:57 +0100

    Updated tree branching, added choose animal menu, fixes, added camera pause

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -7,14 +7,27 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private GameObject cameraFollow;
-
+    public static bool enableFollow = true;
 
     // Update is called once per frame
     void Update()
     {
-        //if(GameManager.Instance.currentAnimal == thisOrganism)
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 temp = new Vector3(ray.origin.x, ray.origin.y, 0f);
-        cameraFollow.transform.position = temp;
+        if (enableFollow)
+        {
+            //if(GameManager.Instance.currentAnimal == thisOrganism)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 temp = new Vector3(ray.origin.x, ray.origin.y, 0f);
+            cameraFollow.transform.position = temp;
+        }
+    }
+
+    public static void ToggleFollowStatic()
+    {
+        enableFollow = !enableFollow;
+    }
+
+    public void ToggleFollow()
+    {
+        enableFollow = !enableFollow;
     }
 }
